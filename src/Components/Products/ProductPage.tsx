06bdfc6ed12900ef348325e57d45ade83f9cfce6
Shane Upchurch <shaneupchurch@gmail.com>
2021-05-11 16:36:04 -0500

    adds add button to products page

--- src/Components/Products/ProductPage.tsx
+++ src/Components/Products/ProductPage.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import ItemDisplay from './ItemDisplay';
 import fruits from '../../Data/fruit-data.js';
 import styles from './ProductPage.module.css';
 
 const ProductPage: React.FC = () => {
 
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    const element = e.currentTarget as HTMLButtonElement
+    const value = element.value
+    
+  }
+
   const fruitElements = fruits.map(fruit => (
     <li key={fruit.id}
         style={{listStyleType: 'none'}}
       >
-      <ItemDisplay {...fruit} />
+      <ItemDisplay fruit={fruit} handleClick={handleClick} />
     </li>
   ))
 
